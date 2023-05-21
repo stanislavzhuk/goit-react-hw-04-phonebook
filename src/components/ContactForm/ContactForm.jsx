addae27b52ea5f468ec2626fe57e4905c75c7115
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-21 17:54:42 +0300

    Update ContactForm.jsx
    The code has been refactored using React hooks.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,83 +1,71 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Form, Label, Input, Button } from "./ContactForm.styled";
 import PropTypes from 'prop-types';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  onChange = evt => {
-    const { name, value } = evt.currentTarget;
+  const onChange = e => {
+    const { name, value } = e.currentTarget;
+    if (name === 'name') setName(value);
     if (name === 'number') {
-      // Phone number formatting
       const phoneNumber = value.replace(/[^\d]/g, '').slice(0, 10);
       const match = phoneNumber.match(/^(\d{3})(\d{3})(\d{2})(\d{2})$/);
       const formattedPhoneNumber = match
         ? `(${match[1]}) ${match[2]}-${match[3]}-${match[4]}`
         : phoneNumber;
-      this.setState({ [name]: formattedPhoneNumber });
-    } else {
-      this.setState({ [name]: value });
-    }
-  };
-  
-  onSubmit = evt => {
-    evt.preventDefault();
-    const { name, number } = this.state;
-    this.props.onSubmit(name, number);
-
-    this.reset();
+      setNumber(formattedPhoneNumber);
+    };
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
+  const onSubmitInput = e => {
+    e.preventDefault();
+    onSubmit(name, number);
+    reset();
+  };
 
-    return (
-      <Form onSubmit={this.onSubmit}>
-        <Label>
-          Name
-          <Input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.onChange}
-            pattern="^[a-zA-Zа-яА-ЯіІїЇєЄґҐ]+(([' -][a-zA-Zа-яА-ЯіІїЇєЄґҐ ])?[a-zA-Zа-яА-ЯіІїЇєЄґҐ]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            placeholder="Enter contact name"
-            required
-          />
-        </Label>
-        <Label>
-          Number
-          <Input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.onChange}
-            pattern="\(\d{3}\) \d{3}-\d{2}-\d{2}"
-            title="Pone number must consist of 10 digits. For exemple 0987654321"
-            placeholder="(XXX) XXX XX XX"
-            maxLength="10"
-            required
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={onSubmitInput}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={onChange}
+          pattern="^[a-zA-Zа-яА-ЯіІїЇєЄґҐ]+(([' -][a-zA-Zа-яА-ЯіІїЇєЄґҐ ])?[a-zA-Zа-яА-ЯіІїЇєЄґҐ]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          placeholder="Enter contact name"
+          required
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={onChange}
+          pattern="\(\d{3}\) \d{3}-\d{2}-\d{2}"
+          title="Pone number must consist of 10 digits. For exemple 0987654321"
+          placeholder="(XXX) XXX XX XX"
+          maxLength="10"
+          required
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
 }
-
-export default ContactForm;
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
